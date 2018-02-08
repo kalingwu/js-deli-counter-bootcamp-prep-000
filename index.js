44033d0cbf5ba7682ddf366bbf580616da1b0266
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-08 18:19:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   }
   else {
-    for (var i = 0; i<katzDeliLine.length)
+    for (var i = 0; i<katzDeliLine.length; i++)
     return "Currently serving "+ name + "."
   }
 }
