196b178b3659c90f237516cb2c1c196141c7d675
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-08 18:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
-
-
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    var name = katzDeliLine.shift()
+    return "Currently serving "+ name + "."
+  }
+}
 
 
 
