4699fb0be1b93bd1d57fe69ce9a26b093e4631b1
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-08 18:08:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,65 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  var position = katzDeliLine.length + 1
+  katzDeliLine.push(name)
+  return 'Welcome, ' + name + '. You are number ' + position + ' in line.'
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+        katzDeliLine.shift()
+    return "Currently serving " + katzDeliLine[0] + "."
+
+  }
+}
+
+function nowServing (deliLine){
+ var name;
+ 
+ name = deliLine.shift()
+ return "Currently serving "+ name + "."
+ }
+
+
+
+
+/*global describe, it */
+/*
+describe('deli', () => {
+  describe('takeANumber', () => {
+    var katzDeli;
+    var otherDeli;
+
+    beforeEach(() => {
+      katzDeli = [];
+      otherDeli = ["Steven", "Blake", "Avi"];
+    });
+
+
+  describe('nowServing', () => {
+    it('returns the line is empty when no one is on line', () => {
+      expect(nowServing([])).toEqual("There is nobody waiting to be served!");
+    });
+
+    it('returns an announcement about the person it is serving, and shifts the line', () => {
+      const deliLine = ["Steven", "Blake", "Avi"]
+      expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
+      expect(deliLine).toEqual(["Blake", "Avi"]);
+    });
+  });
+
+  describe('currentLine(line)', () => {
+    it('returns "The line is currently empty." if no one is in line', () => {
+      expect(currentLine([])).toEqual("The line is currently empty.");
+    });
+
+    it('says who is in line when there are people waiting', () => {
+      expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+    });
+  });
+})
+*/
