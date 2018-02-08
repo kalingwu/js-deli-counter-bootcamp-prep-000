0763719d41b19d76a902bc107856190f59ef7f0f
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-08 18:17:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function nowServing(katzDeliLine) {
+function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
